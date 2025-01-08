3c5882b13c3fff88aeb0bbd652a675e5646c9007
AndreiKrai <okraichenko.a@gmail.com>
2025-01-08 12:28:44 +0100

    slide,fade animation

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Fade, Typography } from "@mui/material";
 import face from "../assets/face.jpg";
 
 const HeroSection = () => {
   const fullText = "Weelcome to my site!";
   const [displayedText, setDisplayedText] = useState("");
+  const [visible, setVisible] = useState(false);
 
   useEffect(() => {
+    // Show the component on mount
+    setTimeout(() => {
+      setVisible(true);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (!visible) {
+      return;
+    }
     let index = 0;
     const interval = setInterval(() => {
       setDisplayedText((prev) => prev + fullText[index]); // Use previous state
@@ -17,13 +28,13 @@ const HeroSection = () => {
     }, 150); // Adjust timing for effect speed
 
     return () => clearInterval(interval); // Cleanup on component unmount
-  }, []);
+  }, [visible]);
   return (
     <Container
-      // sx={{ boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)", // Bottom shadow
-      //   backgroundColor: "white", // Ensure the container has a background to make the shadow visible
-      //  // marginBottom: "4px", // Optional for spacing 
-      //   }}
+    // sx={{ boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)", // Bottom shadow
+    //   backgroundColor: "white", // Ensure the container has a background to make the shadow visible
+    //  // marginBottom: "4px", // Optional for spacing
+    //   }}
     >
       <Box
         sx={{
@@ -32,19 +43,21 @@ const HeroSection = () => {
           alignItems: "center", // Center content
           justifyContent: "center",
           width: "100%", // Ensure container takes full width
-          pb:2
+          pb: 2,
         }}
       >
-        <img
-          src={face}
-          alt="Welcome"
-          style={{
-            maxWidth: "100%", // Image scales with container
-            height: "auto", // Maintains aspect ratio
-            marginBottom: "16px", // Space between image and text
-            marginTop: "10px",
-          }}
-        />
+        <Fade in={visible} timeout={{ appear: 1000, enter: 1000, exit: 1500 }}>
+          <img
+            src={face}
+            alt="Welcome"
+            style={{
+              maxWidth: "100%", // Image scales with container
+              height: "auto", // Maintains aspect ratio
+              marginBottom: "16px", // Space between image and text
+              marginTop: "10px",
+            }}
+          />
+        </Fade>
         <Typography variant="h3" sx={{ color: "black", textAlign: "center" }}>
           {displayedText}
         </Typography>
--- src/components/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -11,10 +11,18 @@ import Container from "@mui/material/Container";
 import MenuItem from "@mui/material/MenuItem";
 import { SvgIcon } from "@mui/material";
 import { pages } from "../navigationData.ts";
-
+import Slide from "@mui/material/Slide";
+import Fade from "@mui/material/Fade";
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function ResponsiveAppBar() {
+  const [show, setShow] =  useState(false);
+
+  useEffect(() => {
+    // Trigger the slide animation on mount
+    setShow(true);
+  }, []);
+
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
     null
   );
@@ -38,14 +46,15 @@ function ResponsiveAppBar() {
   };
 
   return (
+     <Slide direction="down" in={show} timeout={1000} mountOnEnter unmountOnExit>
     <AppBar position="static" sx={{ backgroundColor: "black" }}>
-      <Container maxWidth="xl">
+      <Container maxWidth="xl"  >
         <Toolbar
           disableGutters
           sx={{
             height: { xs: 62, md: 92 },
             flexGrow: 1,
-            justifyContent: "space-between",
+            justifyContent: "space-between",px:2
           }}
         >
           <SvgIcon
@@ -53,7 +62,7 @@ function ResponsiveAppBar() {
           >
             <path d="M11.25 23.991a16.994 16.994 0 0 1-1.384-.171c-4.813-.874-8.652-4.622-9.63-9.401-.184-.904-.218-1.275-.218-2.422 0-1.147.034-1.518.218-2.422A12.06 12.06 0 0 1 10.688.053c.592-.07 2.047-.07 2.61 0 1.728.215 3.226.702 4.631 1.505.447.255 1.143.73 1.57 1.07.502.4 1.458 1.357 1.856 1.857a12.082 12.082 0 0 1 2.59 6.344c.05.472.05 1.864 0 2.336a12.082 12.082 0 0 1-2.59 6.345c-.398.5-1.354 1.456-1.857 1.857-1.799 1.434-3.836 2.282-6.171 2.57-.343.043-1.782.08-2.077.054zm1.832-1.827c1.74-.206 3.238-.76 4.656-1.724a10.218 10.218 0 0 0 4.325-6.731c.11-.657.13-.925.13-1.712 0-.787-.02-1.054-.13-1.711-.711-4.255-4.11-7.655-8.366-8.366-.641-.107-.923-.129-1.683-.13-.953-.002-1.594.07-2.428.274-3.846.94-6.837 4.053-7.604 7.914-.158.798-.176 1-.176 2.02 0 1.018.018 1.22.176 2.018.812 4.088 4.125 7.327 8.23 8.045.214.037.539.082.721.1.182.017.377.036.433.043.181.021 1.45-.008 1.716-.04zm-4.802-5.57a4.425 4.425 0 0 1-.952-.203c-1.96-.625-3.293-2.547-3.191-4.597.104-2.097 1.593-3.866 3.626-4.306 2.662-.577 5.227 1.262 5.565 3.988a5.52 5.52 0 0 1-.059 1.437 4.664 4.664 0 0 1-.697 1.657c-.278.418-.847.99-1.25 1.257a4.614 4.614 0 0 1-3.042.768zm1.038-1.83c1.062-.223 1.873-1.003 2.17-2.084.08-.296.088-1.015.014-1.31a2.887 2.887 0 0 0-1.587-1.94c-.418-.193-.61-.23-1.174-.23-.445 0-.532.008-.735.068a2.862 2.862 0 0 0-1.759 1.424 2.822 2.822 0 0 0 3.071 4.073zm6.26-.443c-.845-1.268-1.532-2.318-1.526-2.333.006-.016.698-1.061 1.539-2.323l1.528-2.295h1.074c.591-.001 1.075.005 1.075.013s-.69 1.048-1.53 2.31c-.843 1.263-1.525 2.312-1.517 2.333.008.02.688 1.048 1.51 2.283a955.44 955.44 0 0 1 1.518 2.281c.018.029-.201.036-1.056.036h-1.079l-1.536-2.305z" />
           </SvgIcon>
-          <Box
+          {/* <Box
             sx={{
               display: { xs: "none", md: "flex" },
               justifyContent: "center",
@@ -63,18 +72,20 @@ function ResponsiveAppBar() {
           >
             {pages.map((page) => (
               <MenuItem key={page.title} onClick={handleCloseNavMenu}>
-                <Typography sx={{ textAlign: "center" }}>{page.title}</Typography>
+                <Typography sx={{ textAlign: "center" }}>
+                  {page.title}
+                </Typography>
               </MenuItem>
             ))}
-          </Box>
+          </Box> */}
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{ display: { md: "none" } }}
+            sx={{ display: { md: "none"} }}
           >
-            <MenuIcon />
+            <MenuIcon sx={{ fontSize: "32px" }}/>
           </IconButton>
           <Box sx={{ display: { xs: "none", md: "block" } }}>
             <IconButton
@@ -88,7 +99,7 @@ function ResponsiveAppBar() {
                 height: { xs: 40, sm: 62 },
               }}
             >
-              <FacebookIcon />
+              <FacebookIcon sx={{ fontSize: "40px" }}/>
             </IconButton>
             <IconButton
               size="large"
@@ -96,12 +107,12 @@ function ResponsiveAppBar() {
               color="inherit"
               aria-label="open drawer"
             >
-              <GitHubIcon />
+              <GitHubIcon  sx={{ fontSize: "40px" }}/>
             </IconButton>
           </Box>
         </Toolbar>
       </Container>
-    </AppBar>
+    </AppBar></Slide>
   );
 }
 export default ResponsiveAppBar;
