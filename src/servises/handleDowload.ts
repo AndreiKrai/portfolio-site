7423a944107794cecc3752eff441d466a1abf870
AndreiKrai <okraichenko.a@gmail.com>
2026-03-26 21:45:05 +0100

    Update dependencies and improve file handling
    - Added @types/node to devDependencies in package.json and package-lock.json
    - Updated undici-types version in package-lock.json
    - Changed file URL handling in handleDownload function for better path resolution
    - Removed CV PDF file from assets
    - Added tsconfig.json for TypeScript configuration

--- src/servises/handleDowload.ts
+++ src/servises/handleDowload.ts
@@ -1,5 +1,5 @@
 const handleDownload = () => {
-    const fileUrl = "../assets/Andrii Okraichenko CV.pdf"; // Path to your PDF file in the public folder
+    const fileUrl = `${process.env.PUBLIC_URL}/assets/Andrii%20Okraichenko%20CV.pdf`; // Path to your PDF file in the public folder
     const link = document.createElement("a");
     link.href = fileUrl;
     link.download = "Andrii Okraichenko FullStack CV.pdf"; // File name for the download
